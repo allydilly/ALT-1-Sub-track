23e6d9674f777f49ad140a3eac5991c600dee475
Glitch (glitch-blank-website) <none>
2021-10-05 14:50:57 +0000

    😹🍜 Checkpoint ./contact1.html:3074250/292 ./contact1.js:3074250/892

--- contact1.js
+++ contact1.js
@@ -1,5 +1,25 @@
 console.log('Contacts log');
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDm658kpc77IYcNGtOw5iMyNPZEK8zQ15Q",
+  authDomain: "nw5-test-aa08c.firebaseapp.com",
+  databaseURL: "https://nw5-test-aa08c-default-rtdb.firebaseio.com",
+  projectId: "nw5-test-aa08c",
+  storageBucket: "nw5-test-aa08c.appspot.com",
+  messagingSenderId: "250665852303",
+  appId: "1:250665852303:web:9d9dad8e6e6fc9cb7c7ffa"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Retrieve the database handle
+const myDBCxn = firebase.database().ref('/contacts');
+
+myDBCxn.on("child_added", displayData);
+
+
 //alert("Welcome to the Contact Us page");
 
 // Tell JavaScript to call saveContacts when SUBMIT button is clicked
@@ -20,8 +40,18 @@ function saveContacts() {
   emailField.value = ''; // clear the field
   emailField.focus(); // set the focus
 
+  // code to save the data to Firebase GOES HERE!
+  const data = myDBCxn.push();
+  data.set( {email: emailFieldValue
+            });
 
 
+}
+
+
+function displayData(data, prevChildKey) {
+    const datapoint = data.val();
+    document.getElementById("qqqq").value += datapoint.email + "\n";
 }
 
 
