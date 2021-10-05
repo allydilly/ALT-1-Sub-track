74a6a0d375d82e5ef95f2a2bd4be5f88d139e388
Glitch (glitch-blank-website) <none>
2021-10-05 14:27:56 +0000

    🎎🍳 Checkpoint ./contact1.html:3074250/36 ./contact1.js:3074250/598

--- contact1.js
+++ contact1.js
@@ -2,6 +2,28 @@ console.log('Contacts log');
 
 //alert("Welcome to the Contact Us page");
 
+// Tell JavaScript to call saveContacts when SUBMIT button is clicked
+const btn = document.getElementById("submit-data");
+btn.addEventListener("click", saveContacts);
+
+// Submit clicked so post the data to the server
+function saveContacts() {
+  
+  //alert("SUBMIT has just been clicked!!!");
+  
+  // read the data from the email field
+  const emailField = document.getElementById("email");
+  const emailFieldValue = emailField.value;
+  alert(emailFieldValue)
+  
+  // reset form 
+  emailField.value = ''; // clear the field
+  emailField.focus(); // set the focus
+
+
+
+}
+
 
 
 
