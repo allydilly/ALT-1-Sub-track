8eb7125e33910a69f56f355c1a9c78ed7570cb83
Glitch (glitch-blank-website) <none>
2021-10-06 14:41:35 +0000

    🐺💫 Checkpoint ./contact1.html:3074250/76 ./contact1.js:3074250/664

--- contact1.js
+++ contact1.js
@@ -1,4 +1,19 @@
 //alert("Welcome to the Contact Us page");
+const firebaseConfig = {
+  apiKey: "AIzaSyAbXRGUbGzeCW3XPHBrhWBjde7fC1fy4jI",
+  authDomain: "testproject-139b2.firebaseapp.com",
+  databaseURL: "https://testproject-139b2-default-rtdb.firebaseio.com",
+  projectId: "testproject-139b2",
+  storageBucket: "testproject-139b2.appspot.com",
+  messagingSenderId: "276557844520",
+  appId: "1:276557844520:web:6ef2089e5a4b3f085e651d"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Retrieve the database handle
+const myDBCxn = firebase.database().ref('/contacts');
 
 
 // TASK 4: Include firebase config info here ...
@@ -17,8 +32,12 @@ function saveContacts() {
   // read the data from the email field
   const emailField = document.getElementById("email");
   const emailFieldValue = emailField.value;
-  alert(emailFieldValue)
+  //alert(emailFieldValue)
 
+  // code to save the data to Firebase GOES HERE!
+  const data = myDBCxn.push();
+  data.set( {email: emailFieldValue
+            });
   
 
 }
