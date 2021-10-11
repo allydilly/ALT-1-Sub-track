41754eedb06f422df8d9a1fe45b25ae75f48a402
Glitch (glitch-blank-website) <none>
2021-10-11 07:11:07 +0000

    📉🎠 Checkpoint ./contact1.html:3074250/37 ./contact1.js:3074250/1571

--- contact1.js
+++ contact1.js
@@ -1,53 +1,18 @@
-//alert("Welcome to the Contact Us page");
-const firebaseConfig = {
-  apiKey: "AIzaSyAbXRGUbGzeCW3XPHBrhWBjde7fC1fy4jI",
-  authDomain: "testproject-139b2.firebaseapp.com",
-  databaseURL: "https://testproject-139b2-default-rtdb.firebaseio.com",
-  projectId: "testproject-139b2",
-  storageBucket: "testproject-139b2.appspot.com",
-  messagingSenderId: "276557844520",
-  appId: "1:276557844520:web:6ef2089e5a4b3f085e651d"
-};
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Retrieve the database handle
-const myDBCxn = firebase.database().ref('/contacts');
-
-
-// TASK 4: Include firebase config info here ...
+alert("Welcome to the Contact Us page");
 
 
 
-// TASK 2: Include code to save the data here ..
-// Tell JavaScript to call saveContacts when SUBMIT button is clicked
-const btn = document.getElementById("submit-data");
-btn.addEventListener("click", saveContacts);
-
-// Submit clicked so post the data to the server
-function saveContacts() {
-  //alert("SUBMIT clicked!!!");
-  
-  // read the data from the email field
-  const emailField = document.getElementById("email");
-  const emailFieldValue = emailField.value;
-  //alert(emailFieldValue)
-
-  // code to save the data to Firebase GOES HERE!
-  const data = myDBCxn.push();
-  data.set( {email: emailFieldValue
-            });
-  
-
-}
 
 
 
-// TASK 5 (step 2): Include code to display the data here ..
-myDBCxn.on("child_added", displayData);
 
-function displayData(data, prevChildKey) {
-    const datapoint = data.val();
-    document.getElementById("contacts").value += datapoint.email + "\n";
-}
+
+
+
+// Task 2D - tell JavaScript to call saveContacts when SUBMIT button is clicked
+
+
+
+
+
