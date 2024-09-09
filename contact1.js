09dcbf0be53b30b2fef8ba4575520defb82eb1e7
Glitch (glitch-blank-website) <none>
2024-09-09 08:31:18 +0000

    🍄⛵️ Checkpoint ./contact1.html:70287282/192 ./contact1.js:70287282/454

--- contact1.js
+++ contact1.js
@@ -1,5 +1,5 @@
 
-alert("Welcome to the Contact Us page");
+// alert("Welcome to the Contact Us page");
 
 // TASK 4B – Copy your web app's Firebase configuration here ...
 
@@ -9,7 +9,28 @@ alert("Welcome to the Contact Us page");
 
 
 // Task 2D - Tell JavaScript to call saveContacts when SUBMIT button is clicked
+const btn = document.getElementById("submit-data"); 
 
+btn.addEventListener("click", saveContacts); 
+
+ 
+
+// Submit clicked so post the data to the server 
+
+function saveContacts() { 
+
+ // alert("SUBMIT clicked!!!"); 
+  const emailField = document.getElementById("email"); 
+
+  const emailFieldValue = emailField.value; 
+
+  alert(emailFieldValue) 
+  
+  emailField.value = ''; // clear the field 
+
+  emailField.focus(); // set the focus 
+  
+} 
 
 
 // Task 5B - Code to retrieve and display the data goes here ...
